f6c86dcddd5013aadca38fc7796be6dd8ab3287b
Yash Gupta <gyash21@gmail.com>
2025-05-26 23:45:03 +0530

    initial-one

---
+++ basic.js
@@ -0,0 +1,9 @@
+getBasics = () => {
+   return{
+        name: "Basic",
+        description: "Basic is a simple and straightforward module that provides essential functionalities for basic operations.",
+   }
+        
+}
+
+getBasics(name)
