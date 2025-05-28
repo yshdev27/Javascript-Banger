642bc73701224e1ca53d7c10c452be1fb78f192a
Yash Gupta <gyash21@gmail.com>
2025-05-29 00:00:02 +0530

    basics

--- basic.js
+++ basic.js
@@ -1,9 +1,25 @@
-getBasics = () => {
-   return{
-        name: "Basic",
-        description: "Basic is a simple and straightforward module that provides essential functionalities for basic operations.",
-   }
-        
-}
+let a  = 10;
+let b = 20;
+let c = 4;
 
-getBasics(name)
+
+
+// operators
+
+let sum = a + b; // addition
+let diff = a - b; // subtraction
+let prod = a * b; // multiplication
+
+console.log("Sum:", sum);
+console.log("Difference:", diff);
+console.log("Product:", prod);
+
+// relational operators
+let isEqual = a === b; // equality  
+let isNotEqual = a !== b; // inequality
+
+
+let isGreater = a > b; // greater than
+
+console.log("Is Equal:", isEqual);
+console.log("Is Not Equal:", isNotEqual);
