6b689bc9a3f8cb5fc6428d606942d1bb94ff2924
Yash Gupta <gyash21@gmail.com>
2025-06-04 23:58:37 +0530

    problems and function

---
+++ reversewhile.js
@@ -0,0 +1,6 @@
+let i = 20;
+
+while(i>=1){
+    console.log(i);
+    i--;
+}
---
+++ sumofN.js
@@ -0,0 +1,6 @@
+let sum =0;
+
+for(let i=1; i<=10; i++){
+    sum += i;
+}
+console.log("The sum of first 10 natural numbers is: " + sum);
